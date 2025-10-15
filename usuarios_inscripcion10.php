8025365122aa516b3d265bf3ba2ac811c107a789
Gianna Filipputti <giannalia@hotmail.com>
2025-10-14 23:13:42 -0300

    update

--- usuarios_inscripcion10.php
+++ usuarios_inscripcion10.php
@@ -1,5 +1,4 @@
 <?php
-
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
@@ -23,9 +22,6 @@ $curso = 1;
 
 
 $codigo = "84954598245";
-
-
-
 
 $reporte = new Curso();
 $report = $reporte->reporteAlumnos_3($curso, $estado);
